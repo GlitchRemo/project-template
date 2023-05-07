565a0ab001c18ca3c4f81549df7cd144a6164ef5
Riya <GlitchRemo@users.noreply.github.com>
2023-05-07 19:05:10 +0530

    added feature to create a template of a project with wiring of files

--- src/create_repository.sh
+++ src/create_repository.sh
@@ -1,27 +1,43 @@
 #! /bin/bash
 
 function source_file() {
+  NAME_IN_PASCAL_CASE=$(KEBAB_TO_CAMEL_CASE $1)
+
   echo "
-  const $NAME_IN_KEBAB_CASE = function() {
+  const $NAME_IN_PASCAL_CASE = function() {
   return 1;
 
-  exports.$NAME_IN_KEBAB_CASE = $NAME_IN_KEBAB_CASE
+  exports.$NAME_IN_PASCAL_CASE = $NAME_IN_PASCAL_CASE
 }
 "
 }
 
 function test_file() {
+  NAME_IN_PASCAL_CASE=$(KEBAB_TO_CAMEL_CASE $1)
+  TEST_NAME=$(SPLIT_WORDS_IN_KEBAB_CASE $1)
+
   echo "
   const {describe, it} = require('node:test');
-  const {strictEqual, deepStrictEqual} = require('assert');
-  const {$NAME_IN_KEBAB_CASE} = require('../src/$PROJECT_NAME.js');
+  const {strictEqual} = require('assert');
+  const {$NAME_IN_PASCAL_CASE} = require('../src/$PROJECT_NAME.js');
 
-  describe('', function() {
-  it('', function() {
+  describe('$TEST_NAME', function() {
+  it('should pass', function() {
   strictEqual(1, 1);
+});
+
+it('should fail', function() {
+strictEqual(1, 0);
 });
 });
 "
+}
+
+function gitignore() {
+  echo "
+  *.sw.*
+  DS.*
+  "
 }
 
 function create_Repo() {
@@ -35,16 +51,13 @@ function create_Repo() {
   NAME_IN_KEBAB_CASE=$(node -p "'$PROJECT_NAME'.replace(/-/g, '_')")
 
   # set up source file
-  source_file > src/$PROJECT_NAME.js
+  source_file $PROJECT_NAME > src/$PROJECT_NAME.js
 
   # set up test file
-  test_file > test/$PROJECT_NAME-test.js
+  test_file $PROJECT_NAME > test/$PROJECT_NAME-test.js
 
   # setting up .gitignore file to ignore .swp files
-  echo "
-  *.swp
-  DS.*
-  " > .gitignore
+  gitignore > .gitignore
 
   # git init
   git init
